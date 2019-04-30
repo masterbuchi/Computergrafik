17d9b931b586043ef2631e45b4d16f991364c37d
Florian Buchholz <33148960+masterbuchi@users.noreply.github.com>
2019-04-30 02:03:40 +0200

    Liam

--- CP - Aufgabe 5_1/Billard_Dreieck/oglwidget.cpp
+++ CP - Aufgabe 5_1/Billard_Dreieck/oglwidget.cpp
@@ -76,80 +76,252 @@ void OGLWidget::initializeGL()
 
 }
 
-void OGLWidget::paintGL()
-{
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glOrtho(-10, 10, -10, 10, 0, 200);
-    c = animstep;   // rotate one degree with each step
-    glTranslatef(0,0,-10);
+//double OGLWidget:: AbstandPunktGerade(A,B,C,P) {
 
-    // Apply rotation angles
-    glPushMatrix();
-    glRotatef(-rotx, 1.0f, 0.0f, 0.0f); // Rotate around x axis
-    glRotatef(-roty, 0.0f, 1.0f, 0.0f); // Rotate around y axis
-    glRotatef(rotz, 0.0f, 0.0f, 1.0f); // Rotate around z axis
+//    a = [A,B];
+//            lamda = (C - dot(a,P))/(dot(a,a));
+//            L = lamda * a + P;
 
-    //Apply scaling
-    double scale = zoom/100.0;
-    glScaled( scale, scale, scale ); // Scale along all axis
-    //SetMaterialColor(0,0,0,1);
-
-    double size = 5;
-
-    glPushMatrix();
-    glScaled(size,2,size);
-    Tisch(1.0);
-    glPopMatrix();
-
-
-
-    dt=unfold/200;
-    px+=dx*dt;
-    pz+=dz*dt;
-
-    glPushMatrix();
-    glRotated(winkel,0,1,0);
-
-    glTranslated(px,1,pz);
-
-
-
-    if (px+1.0>size or px-1.0<-size)
-    {
-        winkel = 90;
-        dx =-dx;
-    }
-
-
-    if (pz+1>size or pz-1<-size)  {
-        //glRotated(180.0,1,0,0);
-        dz =-dz;
-    }
-
-    Kugel();
-
-    glPopMatrix();
-    glPopMatrix();
-}
-
-//void OGLWidget::resizeGL(int w, int h)
-//{
-//    glViewport(0,0,w,h);
-//    glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
+//            ergebnis = sqrt((P(1) - L(1))^2 + (P(2) - L(2))^2);
+//            return ergebnis;
 //}
 
-void OGLWidget::mousePressEvent(QMouseEvent *event)
-{
+            void OGLWidget::paintGL()
+    {
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glOrtho(-10, 10, -10, 10, 0, 200);
+        c = animstep;   // rotate one degree with each step
+        glTranslatef(0,0,-10);
+
+        // Apply rotation angles
+        glPushMatrix();
+        glRotatef(-rotx, 1.0f, 0.0f, 0.0f); // Rotate around x axis
+        glRotatef(-roty, 0.0f, 1.0f, 0.0f); // Rotate around y axis
+        glRotatef(rotz, 0.0f, 0.0f, 1.0f); // Rotate around z axis
+
+        //Apply scaling
+        double scale = zoom/100.0;
+        glScaled( scale, scale, scale ); // Scale along all axis
+        //SetMaterialColor(0,0,0,1);
+
+        double size = 5;
+
+        glPushMatrix();
+        glScaled(size,2,size);
+        Tisch(1.0);
+        glPopMatrix();
+
+
+
+        dt=unfold/200;
+        px+=dx*dt;
+        pz+=dz*dt;
+
+        glPushMatrix();
+        glRotated(winkel,0,1,0);
+
+        glTranslated(px,1,pz);
+
+
+
+        if (px+1.0>size or px-1.0<-size)
+        {
+            winkel += 30;
+            dx =-dx;
+        }
+
+
+        if (pz+1>size or pz-1<-size)  {
+            //glRotated(180.0,1,0,0);
+            dz =-dz;
+        }
+
+        Kugel();
+
+        glPopMatrix();
+        glPopMatrix();
+
+
+
+        //    x_m, y_m, radius, v_x, v_y ,a, frames
+
+        //    % Ortsvektor und Richtungsvektor erste Wand
+        //    o_v_1 = [128;10];
+        //    r_v_1 = [118;118];
+
+        //    % Lamdas Variablen
+        //    syms lam1
+
+
+        //    %Geradengleichungen der Wände
+        //    g_w_1 = o_v_1 + lam1 * r_v_1
+
+        //    % Genauigkeit der Linie
+        //    lamda = 0:0.005:1;
+
+        //    % Darstellung der expliziten Strecken werden berechnet
+        //    wand1 = (round(o_v_1 + lamda .* r_v_1))';
+
+
+        //    %Video starten
+        //    v = VideoWriter('video6.avi');
+        //    open(v);
+
+        //    % Für jedes Frame wird ein neuer Kreis erzeugt
+        //    for i = 1:frames-1
+
+
+
+        //        kreis_o = [x_m;y_m];
+
+        //        kreis_r = [v_x;v_y];
+
+        //        syms lam_kreis
+
+
+        //        %aktuelle Geradengleichung
+        //        g_k = kreis_o + lam_kreis * kreis_r;
+
+
+
+        //        %     %Wand 1
+        //        %     g_w_1 = o_v_1 + 1 * r_v_1
+
+        //        % Geradengleichung von Wand 1
+        //        var_A = - r_v_1(1);
+        //        var_B = r_v_1(2);
+        //        var_C = r_v_1(2)*o_v_1(1) - o_v_1(2) * r_v_1(1);
+
+
+        //        % wenn der Abstand vom Mittelpunkt zur Geraden kleiner ist als r
+        //        if AbstandPunktGerade(var_A,var_B,var_C,kreis_o) < radius
+
+
+        //            % Parameter des Schnittpunktes
+        //            [lam_kreis_w, lam1_w] = solve (g_w_1 - g_k);
+
+        //            % Ortsvektor zum Schnittpunkt
+        //            r_s = subs (g_w_1, lam1, lam1_w);
+
+        //            syms lamda_senk_1
+
+        //            % Richtungsvektor der Senkrechten
+        //            % Steht senkrecht zum Richtungsvektor der Wand
+        //            r_v_1_senk = [0 -1; 1 0] * r_v_1;
+
+
+        //            %Gleichung der Senkrechten
+        //            g_w_1_senk = r_s + lamda_senk_1 * r_v_1_senk;
+
+        //            % Einheitsvektoren
+
+        //            r_v_1_0 = r_v_1 / norm (r_v_1);
+        //            r_v_1_senk_0 = r_v_1_senk / norm (r_v_1_senk);
+
+
+        //            %Transformationsmatrix
+        //            m = [r_v_1_0, r_v_1_senk_0];
+
+
+        //            %Richtungsvektor im neuen Koordinatensystem
+        //            kreis_r_t = m' * kreis_r;
+
+
+        //            % Reflexion im neuen Koordinatensystem
+        //            kreis_r_t_refl = [1 0; 0 -1] * kreis_r_t;
+
+        //            % Reflexion im alten Koordinatensystem
+        //            kreis_r_refl = m * kreis_r_t_refl;
+
+        //            v_x = kreis_r_refl(1);
+        //            v_y = kreis_r_refl(2);
+
+
+        //            %[l1, l2] = lamdas(o_v_1,r_v_1,o_kreis,r_kreis);
+
+        //            % %Ortsvektor vom Schnittpunkt
+        //            % r_s_2 = o_kreis + lam_1_2(2) .* r_kreis
+
+
+        //        end
+
+
+
+        //        % Berechnung des neuen Mittelpunktes
+        //        x_m = x_m + v_x;
+        //        y_m = y_m + v_y;
+
+
+
+        //        % Wenn der Kreis rechts gegen den Rand kommt
+        //        if x_m + radius >= 256
+
+        //            % Die Richtung in der sich der Kreis bewegt, wird umgedreht
+        //            v_x = - v_x;
+        //            x_m = 256 - radius;
+        //        end
+
+        //        % Wenn der Kreis links gegen den Rand kommt
+        //        if x_m - radius <= 0
+
+        //            % Die Richtung in der sich der Kreis bewegt, wird umgedreht
+        //            v_x = - v_x;
+        //            x_m = radius;
+        //        end
+
+
+        //        % Wenn der Kreis unten gegen den Rand kommt
+        //        if y_m + radius >= 255
+
+        //            % Die Richtung in der sich der Kreis bewegt, wird umgedreht
+        //            v_y = -v_y;
+        //            y_m = 256 - radius;
+        //        end
+
+        //        % Wenn der Kreis oben gegen den Rand kommt
+        //        if y_m - radius <= 0
+
+        //            % Die Richtung in der sich der Kreis, wird umgedreht
+        //            v_y = -v_y;
+        //            y_m = radius;
+        //        end
+
+
+        //               % Berechnung der Geschwindigkeit v_y
+        //        v_y = v_y + a;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    //void OGLWidget::resizeGL(int w, int h)
+    //{
+    //    glViewport(0,0,w,h);
+    //    glMatrixMode(GL_PROJECTION);
+    //    glLoadIdentity();
+    //    glMatrixMode(GL_MODELVIEW);
+    //    glLoadIdentity();
+    //}
+
+    void OGLWidget::mousePressEvent(QMouseEvent *event)
+    {
     // Upon mouse pressed, we store the current position...
     lastpos = event->pos();
 }
 
-void OGLWidget::mouseMoveEvent(QMouseEvent *event)
-{
+    void OGLWidget::mouseMoveEvent(QMouseEvent *event)
+    {
     // ... and while moving, we calculate the dragging deltas
     // Left button: Rotating around x and y axis
     int dx = (event->buttons() & Qt::LeftButton) ? lastpos.y() - event->y() : 0;
@@ -164,8 +336,8 @@ void OGLWidget::mouseMoveEvent(QMouseEvent *event)
     lastpos = event->pos();
 }
 
-void OGLWidget::keyPressEvent(QKeyEvent *event)
-{
+    void OGLWidget::keyPressEvent(QKeyEvent *event)
+    {
     // This is the delta we want to use for rotating
     const int keyDelta = 10;
 
@@ -173,35 +345,35 @@ void OGLWidget::keyPressEvent(QKeyEvent *event)
     {
     // Up/Down: Rotating around x axis
     case Qt::Key_Up:
-        emit changeRotation( keyDelta, 0, 0 );
-        break;
+    emit changeRotation( keyDelta, 0, 0 );
+    break;
     case Qt::Key_Down:
-        emit changeRotation( -keyDelta, 0, 0 );
-        break;
+    emit changeRotation( -keyDelta, 0, 0 );
+    break;
 
-        // Left/Right: Rotating around y axis
+    // Left/Right: Rotating around y axis
     case Qt::Key_Left:
-        emit changeRotation( 0, keyDelta, 0 );
-        break;
+    emit changeRotation( 0, keyDelta, 0 );
+    break;
     case Qt::Key_Right:
-        emit changeRotation( 0, -keyDelta, 0 );
-        break;
+    emit changeRotation( 0, -keyDelta, 0 );
+    break;
 
-        // Pg up/down: Rotating around z axis
+    // Pg up/down: Rotating around z axis
     case Qt::Key_PageUp:
-        emit changeRotation( 0, 0, keyDelta );
-        break;
+    emit changeRotation( 0, 0, keyDelta );
+    break;
     case Qt::Key_PageDown:
-        emit changeRotation( 0, 0, -keyDelta );
-        break;
+    emit changeRotation( 0, 0, -keyDelta );
+    break;
 
-        // All other will be ignored
+    // All other will be ignored
     default:
-        break;
-    }
+    break;
 }
-void OGLWidget::Kugel()
-{
+}
+    void OGLWidget::Kugel()
+    {
     int n=10; // Anzahl der 1.0ngrade
     int m=20; // Anzahl der Längengrade
     double dalpha=2.0*M_PI/m;
@@ -211,43 +383,43 @@ void OGLWidget::Kugel()
     double x3,x4,y3,y4,z3,z4=0;
 
     for(j=0;j < n ;j+=1)
-        for(k=0;k < m ;k+=1)
-        {
-            x1=cos(j*dbeta)*cos(k*dalpha);
-            y1=sin(j*dbeta);
-            z1=-cos(j*dbeta)*sin(k*dalpha);
-            x2=cos((j+1)*dbeta)*cos(k*dalpha);
-            y2=sin((j+1)*dbeta);
-            z2=-cos((j+1)*dbeta)*sin(k*dalpha);
-            x3=cos((j+1)*dbeta)*cos((k+1)*dalpha);
-            y3=sin((j+1)*dbeta);
-            z3=-cos((j+1)*dbeta)*sin((k+1)*dalpha);
-            x4=cos(j*dbeta)*cos((k+1)*dalpha);
-            y4=sin(j*dbeta);
-            z4=-cos(j*dbeta)*sin((k+1)*dalpha);
-            glBegin(GL_QUADS);
-            glNormal3d(-x1,-y1,-z1);
-            glColor3d(1.0,0.0,1.0);
-            glVertex3d(x1,y1,z1);
-            //          glColor3f(0.0,0.0,1.0);
-            glVertex3d(x2,y2,z2);
-            //          glColor3f(0.0,0.0,1.0);
-            glVertex3d(x3,y3,z3);
-            //          glColor3f(0.0,0.0,1.0);
-            glVertex3d(x4,y4,z4);
-            glEnd();
-            glBegin(GL_QUADS);
-            glNormal3d(-x1,-y1,-z1);
-            //          glColor3f(0.0,0.0,1.0);
-            glVertex3d(x1,-y1,z1);
-            //          glColor3f(0.0,0.0,1.0);
-            glVertex3d(x2,-y2,z2);
-            //          glColor3f(0.0,0.0,1.0);
-            glVertex3d(x3,-y3,z3);
-            //          glColor3f(0.0,0.0,1.0);
-            glVertex3d(x4,-y4,z4);
-            glEnd();
-        }
+            for(k=0;k < m ;k+=1)
+    {
+        x1=cos(j*dbeta)*cos(k*dalpha);
+        y1=sin(j*dbeta);
+        z1=-cos(j*dbeta)*sin(k*dalpha);
+        x2=cos((j+1)*dbeta)*cos(k*dalpha);
+        y2=sin((j+1)*dbeta);
+        z2=-cos((j+1)*dbeta)*sin(k*dalpha);
+        x3=cos((j+1)*dbeta)*cos((k+1)*dalpha);
+        y3=sin((j+1)*dbeta);
+        z3=-cos((j+1)*dbeta)*sin((k+1)*dalpha);
+        x4=cos(j*dbeta)*cos((k+1)*dalpha);
+        y4=sin(j*dbeta);
+        z4=-cos(j*dbeta)*sin((k+1)*dalpha);
+        glBegin(GL_QUADS);
+        glNormal3d(-x1,-y1,-z1);
+        glColor3d(1.0,0.0,1.0);
+        glVertex3d(x1,y1,z1);
+        //          glColor3f(0.0,0.0,1.0);
+        glVertex3d(x2,y2,z2);
+        //          glColor3f(0.0,0.0,1.0);
+        glVertex3d(x3,y3,z3);
+        //          glColor3f(0.0,0.0,1.0);
+        glVertex3d(x4,y4,z4);
+        glEnd();
+        glBegin(GL_QUADS);
+        glNormal3d(-x1,-y1,-z1);
+        //          glColor3f(0.0,0.0,1.0);
+        glVertex3d(x1,-y1,z1);
+        //          glColor3f(0.0,0.0,1.0);
+        glVertex3d(x2,-y2,z2);
+        //          glColor3f(0.0,0.0,1.0);
+        glVertex3d(x3,-y3,z3);
+        //          glColor3f(0.0,0.0,1.0);
+        glVertex3d(x4,-y4,z4);
+        glEnd();
+    }
 }
 void OGLWidget::Tisch(double r)
 {
@@ -255,7 +427,7 @@ void OGLWidget::Tisch(double r)
 
     //Boden
     glBegin(GL_TRIANGLES);
-    glNormal3f(0,-1,0);
+    glNormal3d(0,-1,0);
     glColor3d(0.0,0.0,1.0);
 
     glVertex3d(-a,0,-r);
@@ -267,7 +439,7 @@ void OGLWidget::Tisch(double r)
 
     // Hintere Wand
     glBegin(GL_QUADS);
-    glNormal3f(0,0,-1);
+    glNormal3d(0,0,-1);
     glColor3d(1.0,1.0,0.0);
     glVertex3d(-a,0,-r);
     glVertex3d(-a,1.0,-r);
@@ -277,7 +449,7 @@ void OGLWidget::Tisch(double r)
 
     // Rechte Wand
     glBegin(GL_QUADS);
-    glNormal3f(1,0,0);
+    glNormal3d(1,0,0);
     glColor3d(1.0,0.0,1.0);
     glVertex3d(a,0,-r);
     glVertex3d(a,1.0,-r);
@@ -287,7 +459,7 @@ void OGLWidget::Tisch(double r)
 
     // Linke Wand
     glBegin(GL_QUADS);
-    glNormal3f(-1,0,0);
+    glNormal3d(-1,0,0);
     glColor3d(0.0,1.0,0.0);
     glVertex3d(-a,0,-r);
     glVertex3d(-a,1.0,-r);
---
+++ CP - Aufgabe 5_1_2/Aufgabe_05_Koop_mit_Holthusen/Beispiel_Billard/Beispiel_Billard/oglwidget.h
@@ -0,0 +1,84 @@
+#ifndef OGLWIDGET_H
+#define OGLWIDGET_H
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QMouseEvent>
+#include <QTimer>
+
+
+class OGLWidget : public QOpenGLWidget,
+                  protected QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+    OGLWidget(QWidget *parent = nullptr);
+    ~OGLWidget();
+
+    // Used to rotate object by mouse
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+    // Used to rotate object by keys (requires focus for OpenGL widget)
+    void keyPressEvent(QKeyEvent *event);
+
+signals:
+    // Emitted whenever a change of rotation angles is requested (e.g. by mouse or key)
+    void changeRotation( int dx, int dy, int dz );
+
+public slots:
+    // Set the rotation angles
+    void setRotX( int newrx );
+    void setRotY( int newry );
+    void setRotZ( int newrz );
+    void collide(float ax, float ay, float bx, float by, float cx, float cy);
+
+
+    // Set zoom factor
+    void setZoom( int newzoom );
+
+    // Set unfolding position
+    void setUnfold( int newunfold );
+
+public slots:
+    void stepAnimation();
+
+
+protected:
+    void initializeGL();
+    //void resizeGL(int w, int h);
+    void paintGL();
+
+protected:
+    QTimer* animtimer; // Timer needed to step animation every x msec
+    int animstep;      // Current animation step (used to rotate triangle
+    void Kugel();
+    void Tisch();
+    void SetMaterialColor(int side, double r, double g, double b);
+
+protected:
+    float c;
+    float dt;
+    float px=0.0f;
+    float dx=-1.0f;
+    float py=0.0f;
+    int dy=1;
+    float pz=0.0f;
+    float dz=1.0f;
+
+
+    //Hier wird die Anzahl der Ecken gesetzt.
+    float Kanten=4;
+    float Ecken[4][2];
+
+    int rotx=0;       // Rotation angles (0..360)
+    int roty=0;
+    int rotz=0;
+    int zoom;       // Zoom factor (0..200, 100 for 1:1)
+    float unfold;     // Unfolding position (0..100, 0 closed)
+    QPoint lastpos; // Last position of mouse pressed, used for dragging
+};
+
+
+#endif // OGLWIDGET_H
